3119c1ad01bf0fd718a7ced65d5bc912553b3205
RichardYang <zhongtian.yang@qq.com>
2024-11-12 01:01:31 +0800

    update: add log for debug

--- controller/controller.go
+++ controller/controller.go
@@ -29,6 +29,8 @@ func (c *Controller) RegisterRoutes(app *fiber.App) {
 }
 
 func (c *Controller) accessToken(ctx *fiber.Ctx) error {
+	log.Debugf("Request: %s", ctx.Body())
+	log.Debugf("Query: %v", ctx.Queries())
 	var data dto.AccessTokenRequest
 	if err := ctx.BodyParser(&data); err != nil {
 		log.Error(err)
